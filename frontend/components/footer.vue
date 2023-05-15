e5f9e38c7ebd0ca2531968cfafa9ecef96a3bbd8
georgiabains <georgiabains@hotmail.co.uk>
2023-05-16 00:49:58 +0100

    Styled footer

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -1,5 +1,15 @@
 <template>
-  <p>&copy; Georgia Bains {{ currentYear }}</p>
+  <footer class="footer">
+    <div class="footer__container">
+      <div class="footer__left">
+        <p>&copy; Georgia Bains {{ currentYear }}</p>
+      </div>
+      <ul class="footer__right list--unstyled">
+        <li><a href="https://github.com/georgiabains">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/georgia-bains/">LinkedIn</a></li>
+      </ul>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -11,3 +21,25 @@
     }
   }
 </script>
+
+<style lang="scss">
+  .footer {
+    padding-inline-end: 1rem;
+    padding-inline-start: 1rem;
+
+    &__container {
+      align-items: center;
+      background-color: #BA8F69;
+      border-radius: 0.5em;
+      display: flex;
+      justify-content: space-between;
+      padding: 2rem 3rem;
+      width: 100%;
+    }
+
+    &__right {
+      display: flex;
+      gap: 1rem;
+    }
+  }
+</style>
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
   css: [
     // SCSS file in the project
     '~/assets/style/main.scss', // you should add main.scss somewhere in your app
