65f787e8e0e2b6395a30948a94b9c917977d377b
georgiabains <georgiabains@hotmail.co.uk>
2023-05-16 00:38:46 +0100

    Created default layout

---
+++ frontend/components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>&copy; Georgia Bains {{ currentYear }}</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        currentYear: new Date().getFullYear()
+      }
+    }
+  }
+</script>
---
+++ frontend/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Georgia Bains</h1>
+</template>
+
+<style>
+  h1 {
+    font-weight: 400;
+  }
+</style>
