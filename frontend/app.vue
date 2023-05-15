0b8670f126a8056051066501103736d73463d03a
georgiabains <georgiabains@hotmail.co.uk>
2023-05-15 22:41:50 +0100

    Initial sanity and nuxt build

---
+++ frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script>
+  import "@fontsource/atkinson-hyperlegible";
+  import "@fontsource/castoro"
+  import "@fontsource/pt-mono"
+</script>
