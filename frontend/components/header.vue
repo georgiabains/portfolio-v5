0906478c4af1889887989262e44363f1284d3290
georgiabains <georgiabains@hotmail.co.uk>
2023-05-16 16:26:55 +0100

    Added meta data

--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -4,6 +4,23 @@
   </header>
 </template>
 
+<script setup>
+const title = ref('Georgia Bains Portfolio')
+const description = ref('My portfolio of web development work.')
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  title,
+  meta: [{
+    name: 'description',
+    content: description
+  }]
+})
+</script>
+
+
 <style>
   h1 {
     font-weight: 400;
