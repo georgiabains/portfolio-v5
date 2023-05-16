839e2ef5afabb131ae09926eed05f18db7625c8a
georgiabains <georgiabains@hotmail.co.uk>
2023-05-16 17:00:48 +0100

    Updated glass effect footer background to use a pseudo element

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -1,16 +1,13 @@
 <template>
   <footer class="footer gutter">
     <div class="footer__container">
-      <div class="footer__background"></div>
-      <div class="footer__flex">
-        <div class="footer__left">
-          <p>&copy; Georgia Bains {{ currentYear }}</p>
-        </div>
-        <ul class="footer__right list--unstyled">
-          <li><a href="https://github.com/georgiabains">Github</a></li>
-          <li><a href="https://www.linkedin.com/in/georgia-bains/">LinkedIn</a></li>
-        </ul>
+      <div class="footer__left">
+        <p>&copy; Georgia Bains {{ currentYear }}</p>
       </div>
+      <ul class="footer__right list--unstyled">
+        <li><a href="https://github.com/georgiabains">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/georgia-bains/">LinkedIn</a></li>
+      </ul>
     </div>
   </footer>
 </template>
@@ -28,34 +25,35 @@
 <style lang="scss">
   .footer {
     &__container {
+      align-items: center;
       background-color: #010201;
       border-radius: 0.5em;
       color: #fff;
+      display: flex;
+      justify-content: space-between;
       overflow: hidden;
-      width: 100%;
+      padding: 2rem 3rem;
       position: relative;
+      width: 100%;
+      z-index: 0;
+
+      &::before {
+        content: "";
+        background-image: url('https://images.unsplash.com/photo-1485134532658-d720895a3b5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80');
+        background-size: cover;
+        background-repeat: no-repeat;
+        filter: blur(10px);
+        height: 110%;
+        left: -5%;
+        position: absolute;
+        top: -5%;
+        width: 110%;
+        z-index: -1;
+      }
 
       a {
         color: #fff;
       }
-    }
-
-    &__background {
-      background-image: url('https://images.unsplash.com/photo-1485134532658-d720895a3b5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80');
-      background-size: cover;
-      background-repeat: no-repeat;
-      filter: blur(10px);
-      height: 100%;
-      position: absolute;
-      width: 100%;
-    }
-
-    &__flex {
-      align-items: center;
-      display: flex;
-      justify-content: space-between;
-      padding: 2rem 3rem;
-      position: relative;
     }
 
     &__right {
