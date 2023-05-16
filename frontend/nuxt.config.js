79fee0aa8fcd4406144dafc68abc3e687909d61f
georgiabains <georgiabains@hotmail.co.uk>
2023-05-16 12:30:04 +0100

    Add vercel anayltics

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
     '~/assets/style/main.scss', // you should add main.scss somewhere in your app
   ],
   modules: ['@nuxtjs/sanity'],
+  plugins: [
+    { src: '~/plugins/vercel.js', mode: 'client' }
+  ],
   sanity: {
     projectId: process.env.NUXT_ENV_SANITY_PROJECT_ID,
     apiVersion: '2022-03-25'
