d33a2b40bf8f6aa02dd0f911129f49ff32a1d967
georgiabains <georgiabains@hotmail.co.uk>
2023-06-04 17:35:30 +0100

    Initial about page

---
+++ frontend/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="gutter">
+    <p v-text="'Crafting an inclusive and accessible digital world is my driving force as a frontend developer. My focus is on creating navigable websites with intuitive features that can be interacted with by everyone.'" />
+
+    <p v-text="'Let\'s standardise the web experience, ensuring that it is accessible and navigable for all.'" />
+  </div>
+</template>
