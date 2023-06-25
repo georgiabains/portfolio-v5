b1215dc6cd03dfda05206341bda3ad60448121ec
georgiabains <georgiabains@hotmail.co.uk>
2023-06-26 00:33:54 +0100

    Draft style for about page

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="gutter">
-    <p v-text="'Crafting an inclusive and accessible digital world is my driving force as a frontend developer. My focus is on creating navigable websites with intuitive features that can be interacted with by everyone.'" />
-
-    <p v-text="'Let\'s standardise the web experience, ensuring that it is accessible and navigable for all.'" />
+  <div class="about">
+    <h1 class="about__title gutter--heading margin-reset" v-text="aboutPage.title" />
+    <div class="about__body gutter">
+      <SanityContent 
+        v-if="aboutPage.body" 
+        :blocks="aboutPage.body"
+      />
+    </div>
   </div>
 </template>
+
+<script setup>
+  const aboutPageQuery = groq`
+    *[_type == "about"][0]{
+      ...
+    }
+  `
+
+  const { data: aboutPage } = await useSanityQuery(aboutPageQuery)
+
+  console.log(aboutPage)
+</script>
+
+<style lang="scss">
+  .about {
+    margin: 0 auto;
+    padding-block-start: 4rem;
+    padding-block-end: 10rem;
+
+    &__title {
+      font-size: 6rem;
+      margin-block-start: 2.2rem;
+    }
+
+    &__body {
+      font-size: 2rem;
+      max-width: 850px;
+    }
+  }
+</style>
