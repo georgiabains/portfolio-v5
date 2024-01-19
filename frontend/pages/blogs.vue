9d32aa679628b6a992ffe9587250aec71cd0f014
georgiabains <georgiabains@hotmail.co.uk>
2024-01-19 21:49:02 +0000

    Restyled blogs page heading

--- frontend/pages/blogs.vue
+++ frontend/pages/blogs.vue
@@ -1,10 +1,7 @@
 <template>
   <BlogContainer v-if="blogs" class="blogs" hide-cta :blog-array="blogs">
     <template #copy>
-      <h1
-        class="blogs__title margin-reset gutter gutter--heading"
-        v-text="'All blog posts'"
-      />
+      <h1 class="heading margin-reset" v-text="'All blog posts'" />
     </template>
   </BlogContainer>
 </template>
@@ -26,14 +23,3 @@
 
   const { data: blogs } = await useSanityQuery(postsQuery)
 </script>
-
-<style lang="scss">
-  .blogs {
-    margin-block-start: 0;
-    padding-block-start: 2.5rem;
-
-    &__title {
-      margin-block-start: 1.25rem;
-    }
-  }
-</style>
