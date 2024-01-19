8e4c821db2e5aa43262b19ba1644a9f1635247bc
georgiabains <georgiabains@hotmail.co.uk>
2024-01-19 21:56:49 +0000

    Pull blogs page title from sanity

--- frontend/pages/blogs.vue
+++ frontend/pages/blogs.vue
@@ -1,7 +1,7 @@
 <template>
   <BlogContainer v-if="blogs" class="blogs" hide-cta :blog-array="blogs">
     <template #copy>
-      <h1 class="heading margin-reset" v-text="'All blog posts'" />
+      <h1 class="heading margin-reset" v-text="blogsPage.title" />
     </template>
   </BlogContainer>
 </template>
@@ -21,5 +21,12 @@
     }
   `
 
+  const blogsPageQuery = groq`
+    *[_type == "blogs"][0]{
+      ...
+    }
+  `
+
   const { data: blogs } = await useSanityQuery(postsQuery)
+  const { data: blogsPage } = await useSanityQuery(blogsPageQuery)
 </script>
