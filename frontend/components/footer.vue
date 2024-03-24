35af6f9996f8fbc3e7d898fb0e570d32e72d4986
georgiabains <georgiabains@hotmail.co.uk>
2024-03-24 18:09:05 +0000

    Simplified footer

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -1,68 +1,30 @@
 <template>
   <footer class="footer gutter">
     <div class="footer__container">
-      <div class="footer__background" aria-hidden="true">
-        <img
-          alt=""
-          loading="lazy"
-          role="presentation"
-          src="https://images.unsplash.com/photo-1485134532658-d720895a3b5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-        />
-      </div>
+      <ul class="list--unstyled footer__socials">
+        <li>
+          <a
+            href="https://github.com/georgiabains"
+            class="footer__link"
+            rel="noreferrer noopener"
+          >
+            <span v-text="'Github'" />
+          </a>
+        </li>
 
-      <div class="footer__content">
-        <div class="footer__split">
-          <div class="footer__left">
-            <h2
-              class="footer__display-copy margin-reset"
-              v-text="'Let\'s make the web accessible for all.'"
-            />
-          </div>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/georgia-bains/"
+            class="footer__link"
+            rel="noreferrer noopener"
+          >
+            <span v-text="'LinkedIn'" />
+          </a>
+        </li>
+      </ul>
 
-          <div class="footer__right">
-            <div class="footer__links-group">
-              <h3 class="margin-reset">Pages</h3>
-              <ul class="list--unstyled">
-                <li><a href="/resources">Accessibility resources</a></li>
-                <li><a href="/sitemap.xml">Sitemap</a></li>
-              </ul>
-            </div>
-            <div class="footer__links-group">
-              <h3 class="margin-reset">Socials</h3>
-              <ul class="list--unstyled footer__socials">
-                <li>
-                  <a
-                    href="https://github.com/georgiabains"
-                    rel="noreferrer noopener"
-                  >
-                    <span v-text="'Explore my Github'" />
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="https://www.linkedin.com/in/georgia-bains/"
-                    rel="noreferrer noopener"
-                  >
-                    <span v-text="'Connect on LinkedIn'" />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <ul class="footer__copyright list--unstyled">
-          <li>&copy; Georgia Bains {{ dateRange }}.</li>
-          <li>
-            Using
-            <a
-              href="https://vercel.com/docs/analytics/privacy-policy"
-              rel="noreferrer noopener"
-              v-text="'Vercel\'s Web Analytics'"
-            />
-          </li>
-        </ul>
+      <div class="footer__right">
+        <p class="footer__copyright">&copy; Georgia Bains {{ dateRange }}.</p>
       </div>
     </div>
   </footer>
@@ -95,58 +57,27 @@
 
     color: var(--text-inverse);
     display: grid;
-    padding-block: var(--spacing-7xl);
-    min-height: 100vh;
+    margin-block-end: var(--spacing-4xl);
+    padding-block: var(--spacing-xl);
 
     &__container {
+      align-items: center;
       background-color: var(--background-dark);
       border-radius: var(--border-radius-44);
-      display: grid;
+      display: flex;
+      justify-content: space-between;
       overflow: hidden;
-      padding-block: var(--spacing-7xl) var(--spacing-4xl);
       padding-inline: var(--spacing-4xl);
       position: relative;
     }
 
-    &__content {
-      display: grid;
-      gap: var(--spacing-7xl);
-      grid-template-rows: 3fr minmax(25px, 1fr);
-      height: 100%;
-      position: relative;
+    &__socials {
+      display: flex;
+      gap: var(--spacing-m);
     }
 
-    &__background {
-      filter: blur(40px) brightness(50%);
-      height: 110%;
-      left: -5%;
-      position: absolute;
-      top: -5%;
-      width: 110%;
-
-      img {
-        max-width: unset;
-        min-height: 100%;
-        min-width: 100%;
-        object-fit: cover;
-      }
-    }
-
-    &__split {
-      display: grid;
-      gap: var(--spacing-4xl);
-      grid-template-columns: 1fr 2fr;
-    }
-
-    &__display-copy {
-      font-family: 'Castoro', serif;
-      font-size: var(--font-44);
-    }
-
-    &__links-group {
-      a {
-        color: var(--text-inverse);
-      }
+    &__link {
+      color: var(--text-inverse);
     }
 
     &__copyright {
