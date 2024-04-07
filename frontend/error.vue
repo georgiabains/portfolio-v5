94b5a6e47cbabf7e9f1c0940f3239464d1031ecf
georgiabains <georgiabains@hotmail.co.uk>
2024-04-07 12:42:26 +0100

    Added page for errors

---
+++ frontend/error.vue
@@ -0,0 +1,33 @@
+// layouts/error.vue
+<template>
+  <main>
+    <div class="layout">
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'error',
+    props: {
+      error: {
+        type: Object,
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  main {
+    min-height: 100vh;
+  }
+
+  .layout {
+    align-items: center;
+    display: grid;
+    height: 100%;
+  }
+</style>
