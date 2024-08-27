13a766d404c9ce2fbd0d0c8eeb02aaeaecbd3ab8
georgiabains <georgiabains@hotmail.co.uk>
2024-08-27 23:15:41 +0100

    Add explicit 400 and 700 font weights for atkinson hyperlegible

--- frontend/app.vue
+++ frontend/app.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script>
-  import '@fontsource/atkinson-hyperlegible'
+  import '@fontsource/atkinson-hyperlegible/400.css'
+  import '@fontsource/atkinson-hyperlegible/700.css'
   import '@fontsource/castoro'
   import '@fontsource/chivo'
   import '@fontsource/overpass'
