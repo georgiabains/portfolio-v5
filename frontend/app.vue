b83ae89f6fe8b3f0948bfd87349ff64fa46a58bf
georgiabains <georgiabains@hotmail.co.uk>
2024-08-27 23:16:51 +0100

    Remove unused fonts from import

--- frontend/app.vue
+++ frontend/app.vue
@@ -7,9 +7,6 @@
 <script>
   import '@fontsource/atkinson-hyperlegible/400.css'
   import '@fontsource/atkinson-hyperlegible/700.css'
-  import '@fontsource/castoro'
-  import '@fontsource/chivo'
-  import '@fontsource/overpass'
   import '@fontsource/pt-mono'
   import '@fontsource/poppins/400.css'
   import '@fontsource/poppins/500.css'
