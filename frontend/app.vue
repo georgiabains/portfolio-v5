51798617d6e707ac3f85048ac357cfe85adac353
georgiabains <georgiabains@hotmail.co.uk>
2024-08-30 21:51:01 +0100

    Changed code font family

--- frontend/app.vue
+++ frontend/app.vue
@@ -7,7 +7,7 @@
 <script>
   import '@fontsource/atkinson-hyperlegible/400.css'
   import '@fontsource/atkinson-hyperlegible/700.css'
-  import '@fontsource/pt-mono'
   import '@fontsource/poppins/400.css'
   import '@fontsource/poppins/500.css'
+  import '@fontsource-variable/source-code-pro'
 </script>
